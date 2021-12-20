c9a218d590721e3789ba65ae8d7a1eda4f7074c9
caspar07 <caspar07@ads.uni-passau.de>
2021-12-20 23:24:12 +0100

    Change redirect address to use Constants

--- src/test/java/fim/unipassau/de/scratch1984/integration/ParticipantControllerIntegrationTest.java
+++ src/test/java/fim/unipassau/de/scratch1984/integration/ParticipantControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import fim.unipassau.de.scratch1984.application.service.ParticipantService;
 import fim.unipassau.de.scratch1984.application.service.UserService;
 import fim.unipassau.de.scratch1984.persistence.entity.Participant;
 import fim.unipassau.de.scratch1984.spring.configuration.SecurityTestConfig;
+import fim.unipassau.de.scratch1984.util.Constants;
 import fim.unipassau.de.scratch1984.web.controller.ParticipantController;
 import fim.unipassau.de.scratch1984.web.dto.ExperimentDTO;
 import fim.unipassau.de.scratch1984.web.dto.ParticipantDTO;
@@ -75,7 +76,7 @@ public class ParticipantControllerIntegrationTest {
     private static final String ERROR = "redirect:/error";
     private static final String PARTICIPANT = "participant";
     private static final String REDIRECT_EXPERIMENT = "redirect:/experiment?id=";
-    private static final String REDIRECT_GUI = "redirect:http://localhost:8601?uid=";
+    private static final String REDIRECT_GUI = "redirect:" + Constants.GUI_URL + "?uid=";
     private static final String REDIRECT_FINISH = "redirect:/finish?id=";
     private static final String EXP_ID = "&expid=";
     private static final String RESTART = "&restart=true";
