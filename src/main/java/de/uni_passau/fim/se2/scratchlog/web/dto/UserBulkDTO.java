b3ade94d24901326db7e910967869c2de4b2dd0b
Niklas Obermeier <oberme55@ads.uni-passau.de>
2026-03-04 22:49:54 +0100

    use lombok for UserBulkDTO

--- src/main/java/de/uni_passau/fim/se2/scratchlog/web/dto/UserBulkDTO.java
+++ src/main/java/de/uni_passau/fim/se2/scratchlog/web/dto/UserBulkDTO.java
@@ -25,13 +25,13 @@ import de.uni_passau.fim.se2.scratchlog.util.validation.annotation.Username;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.Objects;
 
 /**
  * A DTO that represents adding multiple participants at once.
@@ -41,6 +41,8 @@ import java.util.Objects;
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class UserBulkDTO {
 
     /**
@@ -68,54 +70,5 @@ public class UserBulkDTO {
      * highest user ID found in the database will be used.
      */
     private boolean startAtOne;
-
-    /**
-     * Constructs a new user bulk DTO with the given attributes.
-     *
-     * @param amount The number of participants to add.
-     * @param language The preferred language.
-     * @param username The username pattern.
-     * @param startAtOne Whether the username numbering should start at one.
-     */
-    public UserBulkDTO(final int amount, final Language language, final String username, final boolean startAtOne) {
-        this.amount = amount;
-        this.language = language;
-        this.username = username;
-        this.startAtOne = startAtOne;
-    }
-
-    /**
-     * Indicates whether some {@code other} user bulk DTO is semantically equal to this user bulk DTO.
-     *
-     * @param other The object to compare this user bulk DTO to.
-     * @return {@code true} iff {@code other} is a semantically equivalent user bulk DTO.
-     */
-    @Override
-    public boolean equals(final Object other) {
-        if (this == other) {
-            return true;
-        }
-
-        if (other == null || getClass() != other.getClass()) {
-            return false;
-        }
-
-        UserBulkDTO that = (UserBulkDTO) other;
-        return amount == that.amount
-                && startAtOne == that.startAtOne
-                && language == that.language
-                && username.equals(that.username);
-    }
-
-    /**
-     * Calculates a hash code for this user bulk DTO for hashing purposes, and to fulfill the
-     * {@link Object#equals(Object)} contract.
-     *
-     * @return The hash code value of the user bulk DTO.
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount, language, username, startAtOne);
-    }
 
 }
