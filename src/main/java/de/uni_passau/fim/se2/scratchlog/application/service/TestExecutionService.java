ed8290f7d8bc8460851f835d1cdf96d0c65c82d2
Benedikt Fein <fein@fim.uni-passau.de>
2026-04-29 20:33:36 +0200

    whisker: queue test execution

---
+++ src/main/java/de/uni_passau/fim/se2/scratchlog/application/service/TestExecutionService.java
@@ -0,0 +1,10 @@
+package de.uni_passau.fim.se2.scratchlog.application.service;
+
+import de.uni_passau.fim.se2.scratchlog.util.Constants;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile(Constants.PROFILE_WHISKER)
+public class TestExecutionService {
+}
