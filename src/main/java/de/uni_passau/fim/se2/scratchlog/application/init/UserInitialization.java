da5e134e1b23f906074270a5622bb87aa3419a24
Benedikt Fein <fein@fim.uni-passau.de>
2026-06-01 09:32:00 +0200

    style: consistently format licence headers
    Closes #151

--- src/main/java/de/uni_passau/fim/se2/scratchlog/application/init/UserInitialization.java
+++ src/main/java/de/uni_passau/fim/se2/scratchlog/application/init/UserInitialization.java
@@ -1,20 +1,22 @@
 /*
- * Copyright (C) 2023 ScratchLog contributors
- *
  * This file is part of ScratchLog.
+ * Licenced under the GPL v3.0 or later.
+ *
+ * SPDX-FileCopyrightText: 2021-2026 Scratchlog contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
  *
  * ScratchLog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at
- * your option) any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * ScratchLog is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * ScratchLog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with ScratchLog. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package de.uni_passau.fim.se2.scratchlog.application.init;
